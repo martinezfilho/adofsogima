1ac507f76225df99f6177ff6a243a37225787082
Rafael-Pereira901 <rafaelpereira901@gmail.com>
2020-04-11 19:04:03 -0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-
+import requests 
+import json
+import os.path
 
 app = Flask(__name__)
 
@@ -8,21 +10,37 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route("/marzyu")
 def marzyu():
     return render_template("marzyu.html")
-
 
 @app.route("/hitek")
 def hitek():
     return render_template("hitek.html")
 
-
 @app.route("/lad")
 def lad():
-    return render_template("lad.html")
+    save_path = r"static/images" 
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, save_path)   
+    url = "https://api.nasa.gov/planetary/apod"
+    key = "?api_key=dtJInnbvOKDr3u16EZe9C6c2XOguzyWXduZgcMe9"
+    request = requests.get(url + key)
+    if request.status_code == 200:
+        dados = request.json()
+        hdimage = dados['hdurl']
+        title = dados['title']
+        date = dados['date']
 
+    #salvando a imagem 
+    request_img = requests.get(hdimage)
+    name_of_file = date
+    complete_path = os.path.join(path, name_of_file + '.jpg')
+
+    with open(complete_path ,"wb") as file:
+        file.write(request_img.content)
+    file.close()
+    return render_template("lad.html")
 
 @app.route("/felipos")
 def felipos():
